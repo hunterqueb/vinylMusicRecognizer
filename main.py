5b1084e6aaeb6ac6cb8eae2968db88a5ff431388
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-09-02 15:43:30 -0400

    recognizer working, need way to record audio now

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from ShazamAPI import Shazam
+from recognizer import Recognizer
 
-mp3_file_content_to_recognize = open('redbone.mp3', 'rb').read()
+mp3_file_content_to_recognize = open('lostCauseRecorded.mp3', 'rb').read()
 
 shazam = Shazam(mp3_file_content_to_recognize)
 recognize_generator = shazam.recognizeSong()
-while True:
-	print(next(recognize_generator)) # current offset & shazam response to recognize requests
+
+try:
+	song = Recognizer.recognize(mp3_file_content_to_recognize)
+	if song is not None:
+		is_success = True
+	else:
+		is_success = False
+except Exception as e:
+	print(e)
+
+print(song.full_title)
